3e9d96a830ed1d30aafdd9507507a61c977b4a35
Samantha <lisamantha8@gmail.com>
2019-02-28 21:56:27 -0800

    add test of file rooting to main method

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,9 @@
  * @author: Samantha Li
  */
 public class Main {
+    public static void main(String[] args) {
+        String data = Utils.readFileAsString("data/2016_Presidential_Results.csv");
+        System.out.println(data);
+    }
 
 }
