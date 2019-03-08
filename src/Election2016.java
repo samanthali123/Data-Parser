ee19162ae872e679766bde35ed5f7d505bc8d745
Samantha <lisamantha8@gmail.com>
2019-03-07 21:05:34 -0800

    created Election2016 class with getters and setters

---
+++ src/Election2016.java
@@ -0,0 +1,29 @@
+public class Election2016 {
+    private double demVotes;        // number of Democratic votes
+    private double gopVotes;        // number of GOP votes
+    private double totalVotes;      // number of total votes
+
+    public double getDemVotes() {
+        return demVotes;
+    }
+
+    public void setDemVotes(double demVotes) {
+        this.demVotes = demVotes;
+    }
+
+    public double getGopVotes() {
+        return gopVotes;
+    }
+
+    public void setGopVotes(double gopVotes) {
+        this.gopVotes = gopVotes;
+    }
+
+    public double getTotalVotes() {
+        return totalVotes;
+    }
+
+    public void setTotalVotes(double totalVotes) {
+        this.totalVotes = totalVotes;
+    }
+}
