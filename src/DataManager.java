3c44292cf0299a5d2717379be00b821f5d28612d
Samantha <lisamantha8@gmail.com>
2019-03-07 20:59:12 -0800

    created DataManager class with getters and setters

---
+++ src/DataManager.java
@@ -0,0 +1,13 @@
+import java.util.List;
+
+public class DataManager {
+    private List<State> states;     //list of states
+
+    public List<State> getStates() {
+        return states;
+    }
+
+    public void setStates(List<State> states) {
+        this.states = states;
+    }
+}
