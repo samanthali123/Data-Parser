544faf3e74b2a5b25f1ac112f5449067bedfcf39
Samantha <lisamantha8@gmail.com>
2019-03-07 21:08:55 -0800

    created Education2016 class with getters and setters

---
+++ src/Education2016.java
@@ -0,0 +1,38 @@
+public class Education2016 {
+    private double noHighSchool;        // percentage of adults without HS diploma (0 to 100)
+    private double emplyedLaborForce;   // percentage of adults with only a HS diploma
+    private double someCollege;         // percentage of adults with some college
+    private double bachelorsOrMore;     // percentage of adults with a bachelors or higher
+
+    public double getNoHighSchool() {
+        return noHighSchool;
+    }
+
+    public void setNoHighSchool(double noHighSchool) {
+        this.noHighSchool = noHighSchool;
+    }
+
+    public double getEmplyedLaborForce() {
+        return emplyedLaborForce;
+    }
+
+    public void setEmplyedLaborForce(double emplyedLaborForce) {
+        this.emplyedLaborForce = emplyedLaborForce;
+    }
+
+    public double getSomeCollege() {
+        return someCollege;
+    }
+
+    public void setSomeCollege(double someCollege) {
+        this.someCollege = someCollege;
+    }
+
+    public double getBachelorsOrMore() {
+        return bachelorsOrMore;
+    }
+
+    public void setBachelorsOrMore(double bachelorsOrMore) {
+        this.bachelorsOrMore = bachelorsOrMore;
+    }
+}
